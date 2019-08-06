7937d109bdc333a7296d0cbc68e8ff779d3e2dad
dalei <dalei@qq.com>
2019-08-06 14:48:21 +0800

    Create ChannelStaff Controller

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Market/Controllers/ChannelStaffController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiliconValley.InformationSystem.Web.Areas.Market.Controllers
+{
+    public class ChannelStaffController : Controller
+    {
+        // GET: Market/ChannelStaff
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
