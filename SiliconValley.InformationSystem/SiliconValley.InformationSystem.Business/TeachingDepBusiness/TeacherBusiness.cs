dffc65a0ceda6a46862fef79eb455517f2ff9574
zhonglunpan <zhonglunpan@qq.com>
2019-08-06 15:17:19 +0800

    new 教学部业务文件家

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Business/TeachingDepBusiness/TeacherBusiness.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconValley.InformationSystem.Business.TeachingDepBusiness
+{
+    using Base_SysManage;
+    using SiliconValley.InformationSystem.Entity.MyEntity;
+   public class TeacherBusiness:BaseBusiness<Teacher>
+    {
+    }
+}
---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Teaching/Controllers/TeacherController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiliconValley.InformationSystem.Web.Areas.Teaching.Controllers
+{
+
+    using SiliconValley.InformationSystem.Business;
+    public class TeacherController : Controller
+    {
+        // GET: Teaching/Teacher
+
+           
+        public ActionResult TeachersInfo()
+        {
+            return View();
+        }
+    }
+}
