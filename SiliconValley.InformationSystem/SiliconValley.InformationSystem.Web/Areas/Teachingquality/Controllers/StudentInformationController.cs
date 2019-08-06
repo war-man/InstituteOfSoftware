60b0e9edfcb41891e17c721cdfb80ad388d0ca80
chen <chen@qq.com>
2019-08-06 14:29:42 +0800

    ee

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Teachingquality/Controllers/StudentInformationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiliconValley.InformationSystem.Web.Areas.Teachingquality.Controllers
+{
+    [CheckLogin]
+    public class StudentInformationController : Controller
+    {
+        // GET: Teachingquality/StudentInformation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
