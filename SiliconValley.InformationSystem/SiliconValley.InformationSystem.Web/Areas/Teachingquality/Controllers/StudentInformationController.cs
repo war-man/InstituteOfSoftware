6b85c793d328c54e6c6e8cc9e717e277a125c842
chen <chen@qq.com>
2019-08-06 17:35:06 +0800

    q

--- SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Teachingquality/Controllers/StudentInformationController.cs
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Teachingquality/Controllers/StudentInformationController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SiliconValley.InformationSystem.Business.StudentManeger;
 
 namespace SiliconValley.InformationSystem.Web.Areas.Teachingquality.Controllers
-{
+{  //学员信息模块
     [CheckLogin]
     public class StudentInformationController : Controller
     {
+        private readonly StudentInformationManeger dbtext;
+        public StudentInformationController()
+        {
+            dbtext = new StudentInformationManeger();
+        }
         // GET: Teachingquality/StudentInformation
         public ActionResult Index()
         {
+            return View();
+        }
+        //学员注册
+        public ActionResult Registeredtrainees()
+        {
+           var x= dbtext.GetList();
             return View();
         }
     }
