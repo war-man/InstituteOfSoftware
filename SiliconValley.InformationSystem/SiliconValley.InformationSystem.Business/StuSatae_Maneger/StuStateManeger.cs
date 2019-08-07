6362cda45ae4afe913c5fa4a0ec7890d5b85216f
tangmin <tangmin@qq.com>
2019-08-07 17:07:26 +0800

    这是一个新的数据

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Business/StuSatae_Maneger/StuStateManeger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiliconValley.InformationSystem.Entity;
+using SiliconValley.InformationSystem.Entity.MyEntity;
+
+namespace SiliconValley.InformationSystem.Business.StuSatae_Maneger
+{
+   public class StuStateManeger:BaseBusiness<StuStatus>
+    {
+
+    }
+}
