27fdb0ce17e74e1767d34fa59dd6885e4505e3f0
dhytest <dhytest@qq.com>
2019-08-07 14:36:37 +0800

    员工表业务类提交

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Business/EmployeesBusiness/EmployeesInfoManage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconValley.InformationSystem.Business.EmployeesBusiness
+{
+    using Base_SysManage;
+    using SiliconValley.InformationSystem.Entity.MyEntity;
+
+    public class EmployeesInfoManage:BaseBusiness<EmployeesInfo>
+    {
+
+    }
+}
