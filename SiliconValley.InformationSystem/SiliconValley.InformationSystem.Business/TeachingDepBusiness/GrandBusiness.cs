05d9a3a833a1836db84da1f49e28301c5fb2ae0a
zhonglunpan <zhonglunpan@qq.com>
2019-08-08 13:43:45 +0800

    新建阶段业务类

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Business/TeachingDepBusiness/GrandBusiness.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconValley.InformationSystem.Business.TeachingDepBusiness
+{
+    using SiliconValley.InformationSystem.Entity.MyEntity;
+    /// <summary>
+    /// 阶段 业务类
+    /// </summary>
+   public class GrandBusiness:BaseBusiness<Grand>
+    {
+
+    }
+}
