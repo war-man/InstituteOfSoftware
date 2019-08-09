5f42a85fbec8c4cee25854ea30d683d993814ee8
dalei <dalei@qq.com>
2019-08-09 17:50:10 +0800

    8-9下午

--- SiliconValley.InformationSystem/SiliconValley.InformationSystem.Entity/Entity/CooperaEnterprises.cs
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Entity/Entity/CooperaEnterprises.cs
@@ -22,7 +22,7 @@ namespace SiliconValley.InformationSystem.Entity.MyEntity
         public string EntPhone { get; set; }
         public Nullable<bool> IsCooper { get; set; }
         public string Remark { get; set; }
-        public Nullable<bool> IsDel { get; set; }
+
         public Nullable<System.DateTime> Date { get; set; }
     }
 }
