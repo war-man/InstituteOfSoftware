bce7693c1b9fd515d9d5214ba5c106095e99d631
chen <chen@qq.com>
2019-08-15 15:11:26 +0800

    aa

--- SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Teachingquality/Controllers/StudentInformationController.cs
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Teachingquality/Controllers/StudentInformationController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-
 using SiliconValley.InformationSystem.Business.StudentBusiness;
 using SiliconValley.InformationSystem.Business.StudentKeepOnRecordBusiness;
 using SiliconValley.InformationSystem.Entity.MyEntity;
