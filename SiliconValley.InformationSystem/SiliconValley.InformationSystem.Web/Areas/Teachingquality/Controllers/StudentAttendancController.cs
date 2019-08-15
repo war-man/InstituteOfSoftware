a5bf6b1e57a0d4334ad982439442b0a41c2d5e93
chen <chen@qq.com>
2019-08-15 15:00:09 +0800

    aaa

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Teachingquality/Controllers/StudentAttendancController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiliconValley.InformationSystem.Web.Areas.Teachingquality.Controllers
+{
+    public class StudentAttendancController : Controller
+    {
+        // GET: Teachingquality/StudentAttendanc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
