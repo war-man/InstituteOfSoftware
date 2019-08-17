a792e4abab835cfbc8f80da0c44e31ed0b3bb6f8
dhytest <dhytest@qq.com>
2019-08-17 15:52:49 +0800

    8.17 first

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Personnelmatters/Controllers/RecruitingDataSummaryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiliconValley.InformationSystem.Web.Areas.Personnelmatters.Controllers
+{
+    public class RecruitingDataSummaryController : Controller
+    {
+        // GET: Personnelmatters/RecruitingDataSummary
+        public ActionResult RecruitIndex()
+        {
+            return View();
+        }
+        public ActionResult GetRecruitData() {
+            return View();
+        }
+    }
+}
