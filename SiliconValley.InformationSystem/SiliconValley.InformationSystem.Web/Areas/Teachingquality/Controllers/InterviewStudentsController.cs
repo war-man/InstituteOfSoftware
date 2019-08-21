a88f60a61d49b68eb285884399d1608015df3140
chen <chen@qq.com>
2019-08-21 14:08:23 +0800

    www

--- SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Teachingquality/Controllers/InterviewStudentsController.cs
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Teachingquality/Controllers/InterviewStudentsController.cs
@@ -1,9 +1,11 @@
 ﻿using SiliconValley.InformationSystem.Business.ClassesBusiness;
 using SiliconValley.InformationSystem.Business.ClassSchedule_Business;
+using SiliconValley.InformationSystem.Business.Common;
 using SiliconValley.InformationSystem.Business.EmployeesBusiness;
 using SiliconValley.InformationSystem.Business.StudentBusiness;
 using SiliconValley.InformationSystem.Business.StudentmanagementBusinsess;
 using SiliconValley.InformationSystem.Entity.MyEntity;
+using SiliconValley.InformationSystem.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -62,12 +64,153 @@ namespace SiliconValley.InformationSystem.Web.Areas.Teachingquality.Controllers
             return Json(data, JsonRequestBehavior.AllowGet);
         }
 
+        [HttpGet]
         //添加 编辑学员谈话记录
         public ActionResult StudIenterEntiy()
         {
-          
-            ViewBag.Name = "登记学员访谈数据";
+            string id = Request.QueryString["id"];
+            if (!string.IsNullOrEmpty(id) && id != "undefined")
+            {
+
+                ViewBag.Name = "修改学员访谈记录";
+                ViewBag.id = id;
+                return View();
+
+            }
+            ViewBag.id = "";
+            ViewBag.Name = "登记学员访谈记录";
             return View();
         }
+
+        //查询数据库是否由重复数据
+        public ActionResult Errnt(InterviewStudents interviewStudents)
+        {
+
+            string tu = interviewStudents.StudentNumberID.Substring(0, interviewStudents.StudentNumberID.Length);
+            //转成数组
+            string[] stu = tu.Split(',');
+            var list = dbtext.GetList();
+            List<InterviewStudents> mylist = new List<InterviewStudents>();
+            for (int i = 0; i < stu.Length; i++)
+            {
+
+                var count = list.Where(a => a.StudentNumberID == stu[i] && a.InterviewTopics == interviewStudents.InterviewTopics && a.Dateofinterview == interviewStudents.Dateofinterview).ToList();
+                if (count.Count > 0)
+                {
+                    mylist.AddRange(count);
+                }
+            }
+
+            return Json(mylist.Select(a => new { Name = student.GetEntity(a.StudentNumberID).Name, StudentID = a.StudentNumberID, Dateofinterview=a.Dateofinterview }), JsonRequestBehavior.AllowGet);
+
+
         }
+
+        //数据提交到数据库
+        [HttpPost]
+        public ActionResult StudIenterEntiy(InterviewStudents interviewStudents)
+        {
+            string EmpNumber = "201908150001";
+            AjaxResult result = null;
+            //多个学号组成后面截取逗号
+            if (interviewStudents.ID>0)
+            {
+                try
+                {
+                    var x = dbtext.GetEntity(interviewStudents.ID);
+                    interviewStudents.InterviewRecorderID = x.InterviewRecorderID;
+                    interviewStudents.Dateofinterview = x.Dateofinterview;
+                    interviewStudents.IsDelete = x.IsDelete;
+                    interviewStudents.StudentNumberID = x.StudentNumberID;
+                    dbtext.Update(interviewStudents);
+                    result = new SuccessResult();
+                    result.Msg = "修改谈话记录成功";
+                    result.Success = true;
+                }
+                catch (Exception ex)
+                {
+
+
+                    result = new ErrorResult();
+                    result.ErrorCode = 500;
+                    result.Success = false;
+                    result.Msg = "服务器错误";
+                    BusHelper.WriteSysLog(ex.Message, Entity.Base_SysManage.EnumType.LogType.编辑数据异常);
+                }
+                return Json(result, JsonRequestBehavior.AllowGet);
+
+            }
+            else
+            {
+                string tu = interviewStudents.StudentNumberID.Substring(0, interviewStudents.StudentNumberID.Length);
+                //转成数组
+                // Session["EmpNumber"].ToString();
+            
+                string[] stu = tu.Split(',');
+              
+                try
+                {
+                    List<InterviewStudents> list = new List<InterviewStudents>();
+                    for (int i = 0; i < stu.Length; i++)
+                    {
+                        InterviewStudents Students = new InterviewStudents();
+                        Students.InterviewTopics = interviewStudents.InterviewTopics;
+                        Students.Dateofinterview = interviewStudents.Dateofinterview;
+                        Students.Interviewcontent = interviewStudents.Interviewcontent;
+                        Students.InterviewRecorderID = EmpNumber;
+                        Students.StudentNumberID = stu[i];
+                        Students.Addtime = DateTime.Now;
+                        Students.IsDelete = false;
+
+                        list.Add(Students);
+                    }
+                    dbtext.BulkInsert(list);
+                    result = new SuccessResult();
+                    result.Msg = "记录成功";
+                    result.Success = true;
+                }
+                catch (Exception ex)
+                {
+
+                    result = new ErrorResult();
+                    result.ErrorCode = 500;
+                    result.Success = false;
+                    result.Msg = "服务器错误";
+                    BusHelper.WriteSysLog(ex.Message, Entity.Base_SysManage.EnumType.LogType.添加数据异常);
+
+                }
+
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+           
+           
+        }
+
+        //编辑页面赋值数据
+        public ActionResult Select(int id)
+        {
+            try
+            {
+                var x = dbtext.GetList().Where(a => a.ID == id && a.IsDelete == false).Select(c => new
+                {
+                    ID = c.ID,
+                    InterviewTopics = c.InterviewTopics,
+                    Interviewcontent = c.Interviewcontent,
+                    Dateofinterview = c.Dateofinterview,
+                    StudentNumberID = student.GetList().Where(w => w.StudentNumber == c.StudentNumberID && w.IsDelete != true).FirstOrDefault().Name
+
+
+                });
+                return Json(x, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                BusHelper.WriteSysLog(ex.Message, Entity.Base_SysManage.EnumType.LogType.添加数据异常);
+                return Json("数据异常", JsonRequestBehavior.AllowGet);
+            }
+        
+        }
+
+
+    }
 }
