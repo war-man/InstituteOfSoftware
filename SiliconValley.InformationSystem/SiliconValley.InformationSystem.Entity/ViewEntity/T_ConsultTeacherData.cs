3779a1abd2eb7ef4e3ed162bb74932964b7e2350
tangmin <tangmin@qq.com>
2019-09-03 15:31:00 +0800

    提交一个方法给小丁

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Entity/ViewEntity/T_ConsultTeacherData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconValley.InformationSystem.Entity.ViewEntity
+{
+   public class T_ConsultTeacherData
+    {
+       public int Id { get; set; }
+       public string EmpName { get; set; }
+       public string Phone { get; set; }
+       public string Education { get; set; }
+        public string BrainImage { get; set; }
+    }
+}
--- SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Market/Controllers/ConsultTeacherController.cs
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Market/Controllers/ConsultTeacherController.cs
@@ -5,24 +5,35 @@ using System.Web;
 using System.Web.Mvc;
 using SiliconValley.InformationSystem.Business.Consult_Business;
 using SiliconValley.InformationSystem.Entity.MyEntity;
+using SiliconValley.InformationSystem.Entity.ViewEntity;
 
 namespace SiliconValley.InformationSystem.Web.Areas.Market.Controllers
 {
     public class ConsultTeacherController : Controller
     {
         ConsultTeacherManeger Ct_Entiry = new ConsultTeacherManeger();
-        // GET: Market/ConsultTeacher/ConsultTeacherIndex
+        // GET: /Market/ConsultTeacher/GetfontTemplateData
         public ActionResult ConsultTeacherIndex()
         {
             //获取所有咨询师数据
             List<ConsultTeacher> c_list = Ct_Entiry.GetList();
-            ViewBag.data = c_list.Select(c => new {
-                EmpName = Ct_Entiry.SeracherEmp( c.Employees_Id).EmpName,
+            ViewBag.data = c_list.Select(c => new T_ConsultTeacherData()
+            {
+                Id=c.Id,
+                EmpName = Ct_Entiry.SeracherEmp(c.Employees_Id).EmpName,
                 Phone = Ct_Entiry.SeracherEmp(c.Employees_Id).Phone,
                 Education = Ct_Entiry.SeracherEmp(c.Employees_Id).Education,
-                ConsultType =c.ConsultType==false?"初中":"高中",
-                BrainImage=c.BrainImage
+                BrainImage = c.BrainImage
             }).ToList();
+            return View();
+        }
+
+        public ActionResult ConsultTeacherDefalut()//int id
+        {
+            //根据咨询师去找员工编号
+            //ConsultTeacher findc= Ct_Entiry.GetEntity(id);
+            //EmployeesInfo finde = Ct_Entiry.SeracherEmp(findc.Employees_Id);
+
             return View();
         }
     }
