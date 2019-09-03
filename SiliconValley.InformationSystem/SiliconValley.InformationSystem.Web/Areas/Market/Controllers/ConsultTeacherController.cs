c83008c15f0333549934353a3c5fea268bdc70c4
tangmin <tangmin@qq.com>
2019-09-03 10:03:46 +0800

    tijiao

--- SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Market/Controllers/ConsultTeacherController.cs
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Market/Controllers/ConsultTeacherController.cs
@@ -5,25 +5,28 @@ using System.Web;
 using System.Web.Mvc;
 using SiliconValley.InformationSystem.Business.Consult_Business;
 using SiliconValley.InformationSystem.Entity.MyEntity;
+using SiliconValley.InformationSystem.Entity.ViewEntity;
 
 namespace SiliconValley.InformationSystem.Web.Areas.Market.Controllers
 {
     public class ConsultTeacherController : Controller
     {
         ConsultTeacherManeger Ct_Entiry = new ConsultTeacherManeger();
-        // GET: Market/ConsultTeacher/ConsultTeacherIndex
+        // GET: /Market/ConsultTeacher/GetfontTemplateData
         public ActionResult ConsultTeacherIndex()
         {
             //获取所有咨询师数据
             List<ConsultTeacher> c_list = Ct_Entiry.GetList();
-            ViewBag.data = c_list.Select(c => new {
-                EmpName = Ct_Entiry.SeracherEmp( c.Employees_Id).EmpName,
+            ViewBag.data = c_list.Select(c => new T_ConsultTeacherData()
+            {
+                EmpName = Ct_Entiry.SeracherEmp(c.Employees_Id).EmpName,
                 Phone = Ct_Entiry.SeracherEmp(c.Employees_Id).Phone,
                 Education = Ct_Entiry.SeracherEmp(c.Employees_Id).Education,
-                ConsultType =c.ConsultType==false?"初中":"高中",
-                BrainImage=c.BrainImage
+                BrainImage = c.BrainImage
             }).ToList();
             return View();
         }
+
+        
     }
 }
