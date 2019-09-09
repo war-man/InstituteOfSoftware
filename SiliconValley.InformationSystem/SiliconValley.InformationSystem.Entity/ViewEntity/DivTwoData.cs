34a1d812d59884c700d044b76e64060cca28f9e7
tangmin <tangmin@qq.com>
2019-09-09 17:40:55 +0800

    tijiao

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Entity/ViewEntity/DivTwoData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconValley.InformationSystem.Entity.ViewEntity
+{
+   public class DivTwoData
+    {
+        /// <summary>
+        /// 分量的Id
+        /// </summary>
+        public int ConsultId { get; set; }
+        /// <summary>
+        /// 咨询师Id
+        /// </summary>
+        public Nullable<int> ConsultTeacherId { get; set; }
+        /// <summary>
+        /// 咨询师名称
+        /// </summary>
+        public string ConsultTeacherName { get; set; }
+        /// <summary>
+        /// 学生姓名
+        /// </summary>
+        public string StudentName
+        {
+            get;set;
+        }
+        /// <summary>
+        /// 学生ID
+        /// </summary>
+        public Nullable<int> StudentId
+        {
+            get; set;
+        }
+    }
+
+    public class ALLDATA
+    {
+        /// <summary>
+        /// 未完成或完成
+        /// </summary>
+        public int Name
+        { get; set; }
+
+        public List<DivTwoData> list { get; set; }
+    }
+}
