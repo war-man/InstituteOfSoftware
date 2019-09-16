0376a05da6162d0214c22ce58b57545500c7d608
chen <chen@qq.com>
2019-09-16 10:25:30 +0800

    1222

--- SiliconValley.InformationSystem/SiliconValley.InformationSystem.Business/EmpTransactionBusiness/MoveTypeManage.cs
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Business/EmpTransactionBusiness/MoveTypeManage.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace SiliconValley.InformationSystem.Business.EmpTransactionBusiness
 {
-    using SiliconValley.InformationSystem.Entity.Entity;
-   public  class MoveTypeManage:BaseBusiness<MoveType>
+
+    using SiliconValley.InformationSystem.Entity.MyEntity;
+
+    public  class MoveTypeManage:BaseBusiness<MoveType>
     {
         /// <summary>
         /// 根据异动类型编号获取异动类型对象
