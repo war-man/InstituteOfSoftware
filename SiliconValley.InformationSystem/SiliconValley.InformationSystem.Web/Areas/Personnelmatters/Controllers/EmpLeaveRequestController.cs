63d83da43ed4c61cb5b6288e0e6a68c375d2b508
dhytest <dhytest@qq.com>
2019-10-31 09:57:05 +0800

    10.31 first

--- SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Personnelmatters/Controllers/EmpLeaveRequestController.cs
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Personnelmatters/Controllers/EmpLeaveRequestController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SiliconValley.InformationSystem.Business.EmployeesBusiness;
+using SiliconValley.InformationSystem.Business.SchoolAttendanceManagementBusiness;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,38 @@ namespace SiliconValley.InformationSystem.Web.Areas.Personnelmatters.Controllers
         public ActionResult LeaveIndex()
         {
             return View();
+        }
+       
+        /// <summary>
+        /// 获取所有请假申请数据
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public ActionResult GetEmpLeaveRequestData(int page, int limit)
+        {
+            LeaveRequestManage lramanage = new LeaveRequestManage();
+            EmployeesInfoManage emanage = new EmployeesInfoManage();
+            var list = lramanage.GetList();
+            var newlist = list.OrderByDescending(s => s.Id).Skip((page - 1) * limit).Take(limit).ToList();
+            var etlist = from e in newlist
+                         select new
+                         {
+                             #region 获取属性值 
+                             e.Id,
+                             empName = emanage.GetInfoByEmpID(e.EmployeeId).EmpName,
+                             esex = emanage.GetInfoByEmpID(e.EmployeeId).Sex,
+
+                             #endregion
+                         };
+            var newobj = new
+            {
+                code = 0,
+                msg = "",
+                count = list.Count(),
+                data = etlist
+            };
+            return Json(newobj, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Personnelmatters/Controllers/EmpSalaryManagementController.cs
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Personnelmatters/Controllers/EmpSalaryManagementController.cs
@@ -51,54 +51,7 @@ namespace SiliconValley.InformationSystem.Web.Areas.Personnelmatters.Controllers
             return Json(newobj,JsonRequestBehavior.AllowGet);
         }
 
-        //绩效考核统计显示
-        public ActionResult PerformanceAssessShow() {
-            return View();
-        }
-        /// <summary>
-        /// 获取绩效考核的数据
-        /// </summary>
-        /// <param name="page"></param>
-        /// <param name="limit"></param>
-        /// <returns></returns>
-        public ActionResult GetPerformanceAssessData(int page,int limit) {
-            MeritsCheckManage mcmanage = new MeritsCheckManage();
-            EmployeesInfoManage emanage = new EmployeesInfoManage();
-            var mclist = mcmanage.GetList();
-            var newlist = mclist.Skip((page - 1) * limit).Take(limit).ToList();
-            var etlist = from e in newlist
-                         select new
-                         {
-                             #region 获取属性值 
-                             e.Id,
-                             e.EmployeeId,
-                             empName = emanage.GetInfoByEmpID(e.EmployeeId).EmpName,
-                             empDept = emanage.GetDept(emanage.GetInfoByEmpID(e.EmployeeId).PositionId).DeptName,
-                             empPosition=emanage.GetPositionByEmpid(e.EmployeeId).PositionName,
-                            e.YearAndMonth,
-                            e.RoutineWork,
-                            e.RoutineWorkPropotion,
-                            e.RoutineWorkFillRate,
-                            e.OtherWork,
-                            e.OtherWorkPropotion,
-                            e.OtherWorkFillRate,
-                            e.SelfReportedScore,
-                            e.SuperiorGrade,
-                            e.FinalGrade,
-                            e.Remark,
-                            e.IsDel
-                             #endregion
-                         };
-            
-            var newobj = new
-            {
-                code = 0,
-                msg = "",
-                count = mclist.Count(),
-                data = etlist
-            };
-
-            return Json(newobj,JsonRequestBehavior.AllowGet);
-        }
+      
+        
     }
 }
