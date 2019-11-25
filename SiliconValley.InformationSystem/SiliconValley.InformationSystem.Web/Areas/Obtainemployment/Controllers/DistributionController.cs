bb950420db8e59de7c740305dd6e7f4adf1c1aac
DaLei88 <2849802427@qq.com>
2019-11-25 17:07:39 +0800

    提交分配

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Obtainemployment/Controllers/DistributionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiliconValley.InformationSystem.Web.Areas.Obtainemployment.Controllers
+{
+    public class DistributionController : Controller
+    {
+        // GET: Obtainemployment/Distribution
+        public ActionResult DistributionIndex()
+        {
+            return View();
+        }
+    }
+}
