d633c2144ded840d6a6d09823034a0220abc83c3
DaLei88 <2849802427@qq.com>
2019-12-11 10:59:01 +0800

    分配学生

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Obtainemployment/Controllers/EmpStaffAndStuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiliconValley.InformationSystem.Web.Areas.Obtainemployment.Controllers
+{
+    public class EmpStaffAndStuController : Controller
+    {
+        // GET: Obtainemployment/EmpStaffAndStu
+        public ActionResult EmpStaffAndStuIndex()
+        {
+            return View();
+        }
+    }
+}
