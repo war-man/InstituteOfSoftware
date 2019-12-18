3ee7137868c0725bdf43ef9a3aeb087033a17ca0
DaLei88 <2849802427@qq.com>
2019-12-18 20:10:47 +0800

    就业报表雏形

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Obtainemployment/Controllers/EmploymentSummaryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiliconValley.InformationSystem.Web.Areas.Obtainemployment.Controllers
+{
+    public class EmploymentSummaryController : Controller
+    {
+        // GET: Obtainemployment/EmploymentSummary
+        public ActionResult EmploymentSummaryIndex()
+        {
+            return View();
+        }
+    }
+}
