ca67e39073aca625539185ec9f8b15919f8cd805
DaLei88 <2849802427@qq.com>
2020-01-06 16:27:57 +0800

    添加路径

--- SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Market/Controllers/ChannelStaffController.cs
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Market/Controllers/ChannelStaffController.cs
@@ -75,6 +75,7 @@ namespace SiliconValley.InformationSystem.Web.Areas.Market.Controllers
         /// <returns></returns>
         public ActionResult ChannelStaffIndex()
         {
+
             return View();
         }
 
@@ -237,57 +238,72 @@ namespace SiliconValley.InformationSystem.Web.Areas.Market.Controllers
                 dbchastaff = new ChannelStaffBusiness();
                 dbempinfo = new EmployeesInfoManage();
 
+                ///如果没有数据，将会把这个员工区域全部删除
                 ///将已经存在的数据进行一个排除，将这个regions 集合中存在的数据进行一个删除，
                 ///判断以前的区域主管是否是当前传入过来的主管id 如果不是进行一个修改数据
                 ///进行添加操作
-                List<string> regions = param0.RegionIDs.Split(',').ToList();
-                List<ChannelArea> areas= dbemparea.GetAreaByChannelID(param0.ChannelStaffID);
-                for (int i = regions.Count - 1; i >= 0; i--)
+                if (string.IsNullOrEmpty(param0.RegionIDs))
                 {
-                    for (int j = areas.Count - 1; j >= 0; j--)
+                    List<ChannelArea> areas = dbemparea.GetAreaByChannelID(param0.ChannelStaffID);
+                    //删除存在数据中现在没有的数据
+                    foreach (var item in areas)
                     {
-                        //排除相同的。
-                        if (int.Parse(regions[i]) == areas[j].RegionID)
-                        {
-                            regions.RemoveAt(i);
-                            areas.RemoveAt(j);
-                            break;
-                        }
-                    }
-                }
-
-                //删除存在数据中现在没有的数据
-                foreach (var item in areas)
-                {
-                    item.IsDel = true;
-                    dbemparea.Update(item);
-                }
-
-
-
-                ///拿取删除过后的数据
-                List<ChannelArea> newareas = dbemparea.GetAreaByChannelID(param0.ChannelStaffID);
-                foreach (var item in newareas)
-                {
-                    if (param0.RegionalDirectorID != item.RegionalDirectorID)
-                    {
-                        item.RegionalDirectorID = param0.RegionalDirectorID;
+                        item.IsDel = true;
                         dbemparea.Update(item);
                     }
                 }
-
-                ///添加
-                foreach (var item in regions)
+                else
                 {
-                    ChannelArea channelArea = new ChannelArea();
-                    channelArea.ChannelStaffID = param0.ChannelStaffID;
-                    channelArea.IsDel = false;
-                    channelArea.RegionalDirectorID = param0.RegionalDirectorID;
-                    channelArea.RegionID = int.Parse(item);
-                    channelArea.StaffAreaDate = DateTime.Now;
-                    dbemparea.Insert(channelArea);
+                    List<string> regions = param0.RegionIDs.Split(',').ToList();
+                    List<ChannelArea> areas = dbemparea.GetAreaByChannelID(param0.ChannelStaffID);
+                    for (int i = regions.Count - 1; i >= 0; i--)
+                    {
+                        for (int j = areas.Count - 1; j >= 0; j--)
+                        {
+                            //排除相同的。
+                            if (int.Parse(regions[i]) == areas[j].RegionID)
+                            {
+                                regions.RemoveAt(i);
+                                areas.RemoveAt(j);
+                                break;
+                            }
+                        }
+                    }
 
+                    //删除存在数据中现在没有的数据
+                    foreach (var item in areas)
+                    {
+                        item.IsDel = true;
+                        dbemparea.Update(item);
+                    }
+
+
+
+                    ///拿取删除过后的数据
+                    List<ChannelArea> newareas = dbemparea.GetAreaByChannelID(param0.ChannelStaffID);
+                    foreach (var item in newareas)
+                    {
+                        if (param0.RegionalDirectorID != item.RegionalDirectorID)
+                        {
+                            item.RegionalDirectorID = param0.RegionalDirectorID;
+                            dbemparea.Update(item);
+                        }
+                    }
+
+                    ///添加
+                    foreach (var item in regions)
+                    {
+                        ChannelArea channelArea = new ChannelArea();
+                        channelArea.ChannelStaffID = param0.ChannelStaffID;
+                        channelArea.IsDel = false;
+                        channelArea.RegionalDirectorID = param0.RegionalDirectorID;
+                        channelArea.RegionID = int.Parse(item);
+                        channelArea.StaffAreaDate = DateTime.Now;
+                        dbemparea.Insert(channelArea);
+
+                    }
                 }
+               
 
                 ajaxResult.Success = true;
             }
@@ -300,153 +316,7 @@ namespace SiliconValley.InformationSystem.Web.Areas.Market.Controllers
         
             return Json(ajaxResult, JsonRequestBehavior.AllowGet);
         }
-        /// <summary>
-        /// 借资/预资页面
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult BorrowMoney()
-        {
-            //现在采用登陆员工为渠道的员工 
-            //存储到session里面的id找到用户对象，用户对象就会有一个属性为员工编号。
 
-            //现在给一个固定数据id为039e3e3891eea-1e885eb2-75a3-4f82-bb9a-570d717f2af4 员工编号为201908160008 姓名：徐宝平 部门：渠道 岗位：主任
-
-            dbempstaff = new EmploymentStaffBusiness();
-
-            Base_UserModel user = Base_UserBusiness.GetCurrentUser();
-
-            //拿员工对象
-            EmployeesInfo employeesInfo = dbempstaff.GetEmployeesInfoByID(user.EmpNumber);
-            //EmployeesInfo employeesInfo = dbempstaff.GetEmployeesInfoByID("201908160008");
-
-            //拿岗位对象 
-            var PositionInfo = dbempstaff.GetPositionByID(employeesInfo.PositionId);
-
-            //拿部门对象
-            var DepInfo = dbempstaff.GetDepartmentByID(PositionInfo.DeptId);
-
-            ViewBag.PositionInfo = PositionInfo;
-            ViewBag.DepInfo = DepInfo;
-            ViewBag.employeesInfo = employeesInfo;
-            dbbeian = new StudentDataKeepAndRecordBusiness();
-            //获取的是他这个备案而且是已经报名的学生集合
-            List<StudentPutOnRecord> mystudentlist = dbbeian.GetrReport(user.EmpNumber);
-            //List<StudentPutOnRecord> mystudentlist = dbbeian.GetrReport("201908160008");
-            //获取这个员工的预资所有的预资单
-            dbprefunding = new PrefundingBusiness();
-            dbperinfo = new PerInfoBusiness();
-            var mydata = dbprefunding.GetAll();
-            List<PerInfo> mrdperInfos = new List<PerInfo>();
-            //根据这个员工的预资单获取他用过的备案编号
-            foreach (var item in mydata)
-            {
-                List<PerInfo> resultdata = dbperinfo.GetPrefundingByID(item.ID);
-                mrdperInfos.AddRange(resultdata);
-            }
-            //排除用过的备案编号
-            for (int i = mystudentlist.Count - 1; i >= 0; i--)
-            {
-                foreach (var item in mrdperInfos)
-                {
-                    if (mystudentlist[i].Id == item.BeianID)
-                    {
-                        mystudentlist.Remove(mystudentlist[i]);
-                    }
-                }
-            }
-            //存放的是这个员工备案数据已经报名的学生但未用这个学生借资过的。
-            ViewBag.Student = Newtonsoft.Json.JsonConvert.SerializeObject(mystudentlist);
-            return View();
-        }
-        /// <summary>
-        /// 普通借资
-        /// </summary>
-        /// <param></param>
-        /// <returns></returns>
-        public ActionResult PublicBorrow(string EmployeeId, float DebitMoney, string BorrowmoneyReason)
-        {
-            dbempinfo = new EmployeesInfoManage();
-            AjaxResult ajaxResult = new AjaxResult();
-            Debit debit = new Debit();
-            debit.date = DateTime.Now;
-            debit.DebitMoney = DebitMoney;
-            debit.Debitwhy = BorrowmoneyReason;
-            debit.EmpNumber = EmployeeId;
-            debit.IsDel = false;
-            debit.Remark = string.Empty;
-            try
-            {
-                dbempinfo.Borrowmoney(debit);
-                BusHelper.WriteSysLog("当员工普通借资的时候，位于Market区域ChannelStaffController控制器中PublicBorrow方法，添加成功。", EnumType.LogType.添加数据);
-                ajaxResult.Data = "";
-                ajaxResult.ErrorCode = 200;
-                ajaxResult.Success = true;
-                ajaxResult.Msg = "借资成功";
-            }
-            catch (Exception ex)
-            {
-                BusHelper.WriteSysLog("当员工普通借资的时候，位于Market区域ChannelStaffController控制器中PublicBorrow方法，添加失败。", EnumType.LogType.添加数据);
-
-            }
-            return Json(ajaxResult, JsonRequestBehavior.AllowGet);
-        }
-
-        /// <summary>
-        /// 预资表添加
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult DoPrefunding(string EmployeeId, float DebitMoney, string strng1)
-        {
-            AjaxResult ajaxResult = new AjaxResult();
-            try
-            {
-                dbprefunding = new PrefundingBusiness();
-                dbperinfo = new PerInfoBusiness();
-                Prefunding myPrefunding = new Prefunding();
-                myPrefunding.EmpNumber = EmployeeId;
-                myPrefunding.IsDel = false;
-                myPrefunding.PerMoney = DebitMoney;
-
-                var datetime = DateTime.Now;
-                var baiozhi = datetime.ToFileTimeUtc().ToString();
-                myPrefunding.PreDate = datetime;
-                myPrefunding.Remark = baiozhi;
-                dbprefunding.Insert(myPrefunding);
-                BusHelper.WriteSysLog("当员工预资的时候，位于Market区域ChannelStaffController控制器中DoPrefunding方法，添加成功。", EnumType.LogType.添加数据);
-                ajaxResult = dbprefunding.Success("添加成功");
-                dbprefunding = new PrefundingBusiness();
-                var mydata = dbprefunding.GetAll();
-                var dudu = mydata.Where(a => a.Remark == baiozhi).FirstOrDefault();
-                List<string> studentnumber= strng1.Split(',').ToList();
-                foreach (var item in studentnumber)
-                {
-                    PerInfo perInfo = new PerInfo();
-                    perInfo.IsDel = false;
-                    perInfo.PreID = dudu.ID;
-                    perInfo.Remark = string.Empty;
-                    perInfo.BeianID = int.Parse(item);
-                    try
-                    {
-                        dbperinfo.Insert(perInfo);
-                        BusHelper.WriteSysLog("当员工预资详细表的时候，位于Market区域ChannelStaffController控制器中DoPrefunding方法，添加成功。", EnumType.LogType.添加数据);
-                        ajaxResult = dbperinfo.Success("添加成功");
-                    }
-                    catch (Exception)
-                    {
-
-                        BusHelper.WriteSysLog("当员工预资详细表的时候，位于Market区域ChannelStaffController控制器中DoPrefunding方法，添加失败。", EnumType.LogType.添加数据);
-                        ajaxResult = dbperinfo.Error("添加失败");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                BusHelper.WriteSysLog("当员工预资的时候，位于Market区域ChannelStaffController控制器中DoPrefunding方法，添加失败。", EnumType.LogType.添加数据);
-                ajaxResult = dbprefunding.Error("添加失败");
-            }
-
-            return Json(ajaxResult, JsonRequestBehavior.AllowGet);
-        }
 
 
 
