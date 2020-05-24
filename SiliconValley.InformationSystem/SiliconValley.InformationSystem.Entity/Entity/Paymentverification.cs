c608a26e46505812cd16520ecbd28929dd0830e3
1245249261 <1245249261@qq.com>
2020-05-24 22:07:28 +0800

    223

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Entity/Entity/Paymentverification.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconValley.InformationSystem.Entity.Entity
+{
+    /// <summary>
+    /// 核对缴费是否成功
+    /// </summary>
+    [Table("Paymentverification")]
+  public  class Paymentverification
+    {
+        [Key]
+       public int id { get; set; }
+        /// <summary>
+        /// 是否成功，null为待审核
+        /// </summary>
+       public bool ?Passornot { get; set; }
+        /// <summary>
+        /// 单号
+        /// </summary>
+       public string OddNumbers { get; set; }
+        /// <summary>
+        /// 审核时间
+        /// </summary>
+       public DateTime? AddDate { get; set; }
+    }
+}
