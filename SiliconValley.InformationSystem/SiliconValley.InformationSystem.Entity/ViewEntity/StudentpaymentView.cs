639fd8ce899ee1ea37c3b4247f40499838f5712b
1245249261 <1245249261@qq.com>
2020-06-10 12:19:12 +0800

    e

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Entity/ViewEntity/StudentpaymentView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconValley.InformationSystem.Entity.ViewEntity
+{
+    [Table("StudentpaymentView")]
+ public  class StudentpaymentView
+    {
+        [Key]
+        public string StudentNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Sex { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+
+        public string identitydocument { get; set; }
+
+        public string ClassName { get; set; }
+
+        public string Headmasters { get; set; }
+    }
+}
