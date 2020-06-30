e5d023885e2b9cd4be2def1ef1c8dbfad1e781d6
1245249261 <1245249261@qq.com>
2020-06-30 15:00:26 +0800

    12

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Entity/Entity/Messagenotification.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconValley.InformationSystem.Entity.Entity
+{
+    /// <summary>
+    /// 系统消息通知
+    /// </summary>
+    [Table("Messagenotification")]
+  public  class Messagenotification
+    {
+        [Key]
+        public int id { get; set; }
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Content { get; set; }
+        /// <summary>
+        /// 发布人
+        /// </summary>
+        public string PublisherEmployeeId { get; set; }
+      
+        
+        /// <summary>
+        /// 添加时间
+        /// </summary>
+        public DateTime Addtime { get; set; }
+
+    }
+}
