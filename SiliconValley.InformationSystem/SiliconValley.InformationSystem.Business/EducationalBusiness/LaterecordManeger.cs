ab98253afca817786cc9a5b3d2414c8b44ba001f
tangmin <tangmin@qq.com>
2020-07-08 09:00:41 +0800

    90

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Business/EducationalBusiness/LaterecordManeger.cs
@@ -0,0 +1,17 @@
+﻿using SiliconValley.InformationSystem.Entity.ViewEntity.TM_Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconValley.InformationSystem.Business.EducationalBusiness
+{
+    /// <summary>
+    /// 迟到记录表
+    /// </summary>
+   public class LaterecordManeger:BaseBusiness<Laterecord>
+    {
+
+    }
+}
---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Entity/ViewEntity/TM_Data/MyViewEntity/LaterecordView.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconValley.InformationSystem.Entity.ViewEntity.TM_Data.MyViewEntity
+{
+    /// <summary>
+    /// 教务迟到登记视图
+    /// </summary>
+   public class LaterecordView
+    {
+       public int Id { get; set; }
+        /// <summary>
+        /// --班级编号
+        /// </summary>
+        public bool Class_Id { get; set; }
+        /// <summary>
+        /// --班级名称
+        /// </summary>
+        public bool ClassNumber { get; set; }
+        /// <summary>
+        /// --班主任是否到场
+        /// </summary>
+        public bool IsHavaHeadMaster { get; set; }
+        /// <summary>
+        /// --任课老师是否到场
+        /// </summary>
+        public bool IsHavaTeacher { get; set; }
+        /// <summary>
+        /// --PPT是否在讲
+        /// </summary>
+       public bool IshavaPPT { get; set; }
+        /// <summary>
+        /// --应到场人数
+        /// </summary>
+        public int PersonCount { get; set; }
+        /// <summary>
+        /// --实到场人数
+        /// </summary>
+        public int PctualCout { get; set; }
+        /// <summary>
+        /// --其他说明
+        /// </summary>
+        public string Reak { get; set; }
+        /// <summary>
+        /// --日期
+        /// </summary>
+        public DateTime Createdate { get; set; }
+    }
+}
