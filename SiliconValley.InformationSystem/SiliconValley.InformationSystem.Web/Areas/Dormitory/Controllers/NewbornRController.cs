6d780ad9dd0b82a1b2fc01c9d1aefed831958b24
2649019045 <2649019045@qq.com>
2020-07-09 17:16:11 +0800

    222

--- SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Dormitory/Controllers/NewbornRController.cs
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/Dormitory/Controllers/NewbornRController.cs
@@ -159,6 +159,7 @@ namespace SiliconValley.InformationSystem.Web.Areas.Dormitory.Controllers
                 count = data.Count(),
                 data = data2
             };
+            
             return Json(returnObj, JsonRequestBehavior.AllowGet);
 
         }
@@ -194,7 +195,7 @@ namespace SiliconValley.InformationSystem.Web.Areas.Dormitory.Controllers
 
             if (string.IsNullOrEmpty(studentName) && string.IsNullOrEmpty(studentNumber))
             {
-                filterData.AddRange(data)
+                filterData.AddRange(data);
             }
             //进行分页
             List<StudentInformation> pagedata = filterData.Skip((page - 1) * limit).Take(limit).ToList();
